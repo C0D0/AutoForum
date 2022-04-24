98ac681caf2a8804a859bcfbdf3838b8314e80cf
Nicky8Larson <razinekatic@gmail.com>
2022-04-24 18:10:24 +0200

    fix home

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -1,14 +1,15 @@
 
 <nav>
-        <div class="logo" href="../assets/image/logo.png"></div>
+        <div class="logo" href="../assets/image/log.png"></div>
         <input type="checkbox" id="checkbox">
         <label for="checkbox" id="icon">
         <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
         <ul>
-            <li><a href="../../index.php">Home</a></li>
+            <li><a href="../../index.php">Accueil</a></li>
             <li><a href="../../vues/v_publier-question.php">Publier une question</a></li>
             <li><a href="../../vues/v_mes-questions.php">Mes questions</a></li>
+            <li><a href="../../vues/v_connexion.php" class="active">Connexion</a></li>
             <?php 
               if(isset($_SESSION['auth'])){
                 ?>

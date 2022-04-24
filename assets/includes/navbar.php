fcacdb791e6e550585834a378723021266a8894c
Nicky8Larson <razinekatic@gmail.com>
2022-04-24 18:58:24 +0200

    connexion fixex

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -1,22 +1,23 @@
-
 <nav>
-        <div class="logo" href="../assets/image/logo.png"></div>
-        <input type="checkbox" id="checkbox">
-        <label for="checkbox" id="icon">
-        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </label>
-        <ul>
-            <li><a href="../../index.php">Accueil</a></li>
-            <li><a href="../../vues/v_publier-question.php">Publier une question</a></li>
-            <li><a href="../../vues/v_mes-questions.php">Mes questions</a></li>
-            <li><a href="../../vues/v_mes-questions.php" class="active">Connexion</a></li>
-            <?php 
-              if(isset($_SESSION['auth'])){
-                ?>
-                  <li><a href="../../vues/v_profile.php?id=<?= $_SESSION['id']; ?>" class="active">Mon profil</a></li>
+  <div class="logo" href="../assets/image/logo.png"></div>
+  <input type="checkbox" id="checkbox">
+  <label for="checkbox" id="icon">
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+    </svg>
+  </label>
+  <ul>
+    <li><a href="../../index.php">Accueil</a></li>
+    <li><a href="../../vues/v_publier-question.php">Publier une question</a></li>
+    <li><a href="../../vues/v_mes-questions.php">Mes questions</a></li>
     
-                  <li><a href="../../controleurs/users/c_deconnexion.php" class="active">Déconnexion</a></li>
-            <?php }
-                ?>
-        </ul>
-    </nav>
+
+    <?php if ((empty($_SESSION['auth']))) { ?>
+      <li><a href="../../vues/v_connexion.php" class="active">Connexion</a></li>
+    <?php } else { ?>
+      <li><a href="../../vues/v_profile.php?id=<?= $_SESSION['id']; ?>" class="active">Mon profil</a></li>
+
+      <li><a href="../../controleurs/users/c_deconnexion.php" class="active">Déconnexion</a></li>
+    <?php } ?>
+  </ul>
+</nav>

96ddd20964165baa038cce8b662624311e5e6148
Nicky8Larson <razinekatic@gmail.com>
2022-04-24 18:13:12 +0200

    fix front

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -6,9 +6,10 @@
         <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
         <ul>
-            <li><a href="../../index.php">Home</a></li>
+            <li><a href="../../index.php">Accueil</a></li>
             <li><a href="../../vues/v_publier-question.php">Publier une question</a></li>
             <li><a href="../../vues/v_mes-questions.php">Mes questions</a></li>
+            <li><a href="../../vues/v_mes-questions.php" class="active">Connexion</a></li>
             <?php 
               if(isset($_SESSION['auth'])){
                 ?>
